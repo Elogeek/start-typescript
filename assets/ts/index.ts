e03ed8486dfa02bedc9f7d6c492248ebe7bad65f
elogeek <elodiechristin@gmail.com>
2021-12-14 15:33:42 +0100

    introduction Typescript

--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,9 +1,10 @@
 // First fct via Typescript
 function hello(name: string, a: number) {
 
-    while(a -->0) {
+    while(a -- > 0) {
         alert(`hello ${a} `);
     }
 }
 
 hello('test',1);
+hello('test', 2);
